9dfcb97e349c2a1c562483bdd086e6109d56d548
unknown <harha@gmail.com>
2025-09-16 17:00:30 +0530

    Build and deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+
 export default defineConfig({
   site: 'https://optyp-com.github.io/optyp.github.io/',
   integrations: [tailwind()],
-  base: '/',
+  base: '/optyp.github.io/',
   build: { assetsPrefix: './' },
   vite: { build: { rollupOptions: { output: { assetFileNames: 'assets/[name].[hash][extname]' }}}}
 });
