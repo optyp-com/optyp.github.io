86cae3409c5868cdee75f4fa5aa2c92df707c9e0
unknown <harha@gmail.com>
2025-09-16 06:46:03 +0530

    cicd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 export default defineConfig({
-  site: 'https://optyp.github.io',
+  site: 'https://optyp-com.github.io/optyp.github.io/',
   integrations: [tailwind()],
   base: '/',
   build: { assetsPrefix: './' },
