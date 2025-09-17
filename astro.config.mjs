34759765553903340b04066236f781d564152cf3
unknown <harha@gmail.com>
2025-09-17 19:13:51 +0530

    index file changed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://optyp-com.github.io/optyp.github.io/',
+  site: 'https://optyp.com', // Use your custom domain
   integrations: [tailwind()],
-  base: '/optyp.github.io/',
-  build: { assetsPrefix: './' },
+  output: 'static',
+  base: '/', // Root base path for your site
   vite: {
     build: {
+      cssCodeSplit: false, // Disable CSS code splitting to avoid missing css files
       rollupOptions: {
         output: {
           assetFileNames: 'assets/[name].[hash][extname]'
