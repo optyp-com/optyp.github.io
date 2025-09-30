e1b764f0b695f1b8b6f4848b0c02bf9fe2c771c0
unknown <harha@gmail.com>
2025-09-30 19:28:05 +0530

    Updates

--- _astro/hoisted.3OgKqnFB.js
+++
@@ -1,1 +0,0 @@
-const e=document.getElementById("backToTop");window.addEventListener("scroll",()=>{window.scrollY>window.innerHeight/2?e.classList.remove("hidden"):e.classList.add("hidden")});e?.addEventListener("click",()=>{window.scrollTo({top:0,behavior:"smooth"})});
